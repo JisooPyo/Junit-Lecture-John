b5c6cf19f1ffb6a04e3a588660fbca0241f897e8
JisooPyo <audwl326@gmail.com>
2024-03-28 16:45:05 +0900

    3-29: Make it

--- src/main/java/com/udemy/junit_lecture_1/Bank.java
+++ src/main/java/com/udemy/junit_lecture_1/Bank.java
@@ -2,6 +2,6 @@ package com.udemy.junit_lecture_1;
 
 public class Bank {
     Money reduce(Expression source, String toCurrency) {
-        return Money.dollar(10);
+        return source.reduce(toCurrency);
     }
 }
--- src/main/java/com/udemy/junit_lecture_1/Expression.java
+++ src/main/java/com/udemy/junit_lecture_1/Expression.java
@@ -1,4 +1,5 @@
 package com.udemy.junit_lecture_1;
 
 public interface Expression {
+    Money reduce(String to);
 }
