1d363f6ee9abc393a2295e304f504ae6ca8e0971
JisooPyo <audwl326@gmail.com>
2024-03-28 17:58:57 +0900

    3-32. Abstraction, Finally

--- src/main/java/com/udemy/junit_lecture_1/Expression.java
+++ src/main/java/com/udemy/junit_lecture_1/Expression.java
@@ -3,4 +3,6 @@ package com.udemy.junit_lecture_1;
 public interface Expression {
     Money reduce(Bank bank, String to);
     Expression plus(Expression addend);
+
+    Expression times(int multiplier);
 }
