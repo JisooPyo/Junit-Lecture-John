a8eaa54737895366a64e77cdcb29ca74af6929e2
JisooPyo <audwl326@gmail.com>
2024-03-28 11:31:56 +0900

    init: 프로젝트 초기 설정 및 README 수정

---
+++ src/main/java/com/udemy/junit_lecture_1/UdemyJunitLecture1Application.java
@@ -0,0 +1,13 @@
+package com.udemy.junit_lecture_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyJunitLecture1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyJunitLecture1Application.class, args);
+	}
+
+}
