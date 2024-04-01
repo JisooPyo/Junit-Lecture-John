54aecc521ff4dcd4bebb0f06ae54867d87bd700d
JisooPyo <audwl326@gmail.com>
2024-04-01 20:51:07 +0900

    5-50. JUnit Dependent Assertions

---
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/model/OwnerTest.java
@@ -0,0 +1,23 @@
+package com.udemy.junit_lecture_1.testJavaWithJunit.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("1231231234");
+
+        assertAll("Properties Test",
+            () -> assertAll("Person Properties",
+                () -> assertEquals("Joe", owner.getFirstName(), "Fist Name Did Not Match"),
+                () -> assertEquals("Buck", owner.getLastName())),
+            () -> assertAll("Owner Properties",
+                () -> assertEquals("Key West", owner.getCity(), "Cith Did Not Match"),
+                () -> assertEquals("1231231234", owner.getTelephone())
+            ));
+    }
+}
