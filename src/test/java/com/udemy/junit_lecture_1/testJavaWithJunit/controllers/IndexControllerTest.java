a2b1e75c65631425c0e5f9b8297a19c59c974296
JisooPyo <audwl326@gmail.com>
2024-04-01 20:59:46 +0900

    5-52. JUnit Test Display Names

--- src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/controllers/IndexControllerTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/controllers/IndexControllerTest.java
@@ -3,6 +3,7 @@ package com.udemy.junit_lecture_1.testJavaWithJunit.controllers;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class IndexControllerTest {
@@ -13,6 +14,7 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Test Proper View name is returned for index page")
     @Test
     void index() {
         assertEquals("index", controller.index());
@@ -27,6 +29,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Test Exception")
     void oupsHandler() {
         assertTrue("notimplemented".equals(controller.oupsHandler()),
             () -> "This is some expensive " +
