dc42153c3221c46341bed4870176319089ec826f
JisooPyo <audwl326@gmail.com>
2024-04-01 20:56:49 +0900

    5-51. Skipping JUnit Tests

---
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,47 @@
+package com.udemy.junit_lecture_1.testJavaWithJunit.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.udemy.junit_lecture_1.testJavaWithJunit.model.Owner;
+
+@Disabled(value = "Disabled until we learn Mocking")
+class OwnerSDJpaServiceTest {
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Disabled
+    @Test
+    void findByLastName() {
+        Owner foundOwner = service.findByLastName("Buck");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
