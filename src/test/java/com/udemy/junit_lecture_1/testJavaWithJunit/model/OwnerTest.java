e8234e09e56e23a6952103267817e9b627016ff7
JisooPyo <audwl326@gmail.com>
2024-04-02 00:02:52 +0900

    5-58. Using Hamcrest with JUnit

--- src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/model/OwnerTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/model/OwnerTest.java
@@ -1,5 +1,7 @@
 package com.udemy.junit_lecture_1.testJavaWithJunit.model;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -19,5 +21,8 @@ class OwnerTest {
                 () -> assertEquals("Key West", owner.getCity(), "Cith Did Not Match"),
                 () -> assertEquals("1231231234", owner.getTelephone())
             ));
+
+        // Hamcrest 라이브러리
+        assertThat(owner.getCity(), is("Key West"));
     }
 }
