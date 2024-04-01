669622a4ecc963e411cf0a7ffe25b391b51c602a
JisooPyo <audwl326@gmail.com>
2024-04-01 21:05:03 +0900

    5-53. Testing Expected Exceptions

--- src/main/java/com/udemy/junit_lecture_1/testJavaWithJunit/controllers/IndexController.java
+++ src/main/java/com/udemy/junit_lecture_1/testJavaWithJunit/controllers/IndexController.java
@@ -2,12 +2,12 @@ package com.udemy.junit_lecture_1.testJavaWithJunit.controllers;
 
 public class IndexController {
 
-    public String index(){
+    public String index() {
 
         return "index";
     }
 
-    public String oupsHandler(){
-        return "notimplemented";
+    public String oopsHandler() {
+        throw new ValueNotFoundException();
     }
 }
---
+++ src/main/java/com/udemy/junit_lecture_1/testJavaWithJunit/controllers/ValueNotFoundException.java
@@ -0,0 +1,4 @@
+package com.udemy.junit_lecture_1.testJavaWithJunit.controllers;
+
+public class ValueNotFoundException extends RuntimeException {
+}
