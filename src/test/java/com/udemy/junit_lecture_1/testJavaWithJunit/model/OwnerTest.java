30053149a2c68ef5ec8c094c5d6e2f9b0c5f63d7
JisooPyo <audwl326@gmail.com>
2024-04-03 13:58:30 +0900

    6-72. JUnit Parameterized Test - Display Name

--- src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/model/OwnerTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/model/OwnerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -30,7 +31,8 @@ class OwnerTest implements ModelTests {
         assertThat(owner.getCity(), is("Key West"));
     }
 
-    @ParameterizedTest
+    @DisplayName("Value Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
     // short, byte, int, long, float, double, char, boolean, String, Class
     @ValueSource(strings = {"Spring", "Framework", "Guru"})
     void testValueSource(String val) {
