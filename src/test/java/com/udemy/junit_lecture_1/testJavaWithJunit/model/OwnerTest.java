afd1561b757f45537844f4345d76473030e10a2b
JisooPyo <audwl326@gmail.com>
2024-04-03 14:21:38 +0900

    6-76. JUnit Parameterized Tests - Method Provider

--- src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/model/OwnerTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/model/OwnerTest.java
@@ -4,12 +4,16 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import com.udemy.junit_lecture_1.testJavaWithJunit.ModelTests;
@@ -65,5 +69,19 @@ class OwnerTest implements ModelTests {
     @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
     void csvFromFileTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " = " + val1 + " : " + val2);
+    }
+
+    @DisplayName("Method Provider Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getArgs")
+    void fromMethodTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + " : " + val2);
+    }
+
+    static Stream<Arguments> getArgs() {
+        return Stream.of(
+            Arguments.of("FL", 4, 4),
+            Arguments.of("OH", 5, 5),
+            Arguments.of("MI", 6, 6));
     }
 }
