7b4d96493dbbcd2629896361ca7de8322fc0196a
JisooPyo <audwl326@gmail.com>
2024-04-03 23:06:49 +0900

    9-106. JUnit Mockito Extension

--- src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/AnnotationMocksTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/AnnotationMocksTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+// Mock 객체를 생성하는 방법 2. 어노테이션 이용 - @Mock, MockitoAnnotations.openMocks(this)로 객체 초기화
 public class AnnotationMocksTest {
 
     @Mock
--- src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/InlineMockTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/InlineMockTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+// Mock 객체를 생성하는 방법 1. mock 메서드 이용 - mock(Class<T>)
 public class InlineMockTest {
     @Test
     void testInlineMock() {
