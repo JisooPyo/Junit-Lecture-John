9ccc49575925b0af3a1f7b7945866369694cf8f5
JisooPyo <audwl326@gmail.com>
2024-04-03 14:31:10 +0900

    6-78. Unit Tests vs Integration Tests

---
+++ src/test/java/com/udemy/junit_lecture_1/testJavaWithJunit/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,37 @@
+package com.udemy.junit_lecture_1.testJavaWithJunit.services.springdatajpa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// 통합 테스트는 암묵적으로
+// 1. 접미사 "IT"를 테스트 클래스 명에 추가하거나
+// 2. 별도의 디렉토리(ex. src/test/integrationTest)에 배치하는
+// 룰이 있다. 꼭 지켜야 하는 것은 아님
+class PetTypeSDJpaServiceIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
