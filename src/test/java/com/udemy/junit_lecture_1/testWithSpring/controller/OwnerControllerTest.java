0ce303bcaf92b2e6587cb1cc25922e694b0d0ec4
JisooPyo <audwl326@gmail.com>
2024-04-10 15:13:32 +0900

    14-164. Using Spring MVC Test with Form Parameters

--- src/test/java/com/udemy/junit_lecture_1/testWithSpring/controller/OwnerControllerTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testWithSpring/controller/OwnerControllerTest.java
@@ -27,6 +27,15 @@ class OwnerControllerTest {
     }
 
     @Test
+    void testFindByNameNotFound() throws Exception {
+        mockMvc.perform(get("/owners")
+                .param("lastName", "Don't find ME!"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("owners/findOwners"));
+
+    }
+
+    @Test
     void initCreationFormTest() throws Exception {
         mockMvc.perform(get("/owners/new"))
             .andExpect(status().isOk())
