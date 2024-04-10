f85774224393b044aaf12779d7fecafb57434bae
JisooPyo <audwl326@gmail.com>
2024-04-10 18:02:47 +0900

    14-168~169. Use Spring MVC Test with Form Post

--- src/test/java/com/udemy/junit_lecture_1/testWithSpring/controller/OwnerControllerTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testWithSpring/controller/OwnerControllerTest.java
@@ -34,10 +34,40 @@ class OwnerControllerTest {
     MockMvc mockMvc;
     @Captor
     ArgumentCaptor<String> stringArgumentCaptor;
+    @Captor
+    ArgumentCaptor<Owner> ownerArgumentCaptor;
 
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void testUpdateOwnerNotValid() throws Exception {
+        mockMvc.perform(post("/owners/{ownerId}/edit", 2)
+                .param("firstName", "Jimmy")
+                .param("lastName", "Buffet")
+                .param("telephone", "3151231234"))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeHasErrors("owner"))
+            .andExpect(model().attributeHasFieldErrors("owner", "city"))
+            .andExpect(model().attributeHasFieldErrors("owner", "address"))
+            .andExpect(view().name("owners/createOrUpdateOwnerForm"));
+    }
+
+    @Test
+    void testUpdateOwnerValid() throws Exception {
+        mockMvc.perform(post("/owners/{ownerId}/edit", 2)
+                .param("firstName", "Jimmy")
+                .param("lastName", "Buffet")
+                .param("address", "123 Duval St")
+                .param("city", "Key West")
+                .param("telephone", "3151231234"))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(view().name("redirect:/owners/{ownerId}"));
+
+        then(clinicService).should().saveOwner(ownerArgumentCaptor.capture());
+        assertThat(ownerArgumentCaptor.getValue().getId()).isEqualTo(2);
     }
 
     @Test
