2002b128c66db9852aa35b76ec1685a9c5cc5629
JisooPyo <audwl326@gmail.com>
2024-04-14 22:39:48 +0900

    16-185. @WebMVC Test Slice

--- src/test/java/com/udemy/junit_lecture_1/testWithSpringBoot/controller/BeerControllerTest.java
+++ src/test/java/com/udemy/junit_lecture_1/testWithSpringBoot/controller/BeerControllerTest.java
@@ -2,45 +2,47 @@ package com.udemy.junit_lecture_1.testWithSpringBoot.controller;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.udemy.junit_lecture_1.testWithSpringBoot.dto.BeerDto;
 import com.udemy.junit_lecture_1.testWithSpringBoot.model.BeerStyleEnum;
 import com.udemy.junit_lecture_1.testWithSpringBoot.service.BeerService;
 
-@ExtendWith(MockitoExtension.class)
-@AutoConfigureMockMvc
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
-    @Mock
-    BeerService beerService;
-    @InjectMocks
-    BeerController controller;
 
+    @MockBean
+    BeerService beerService;
+
+    @Autowired
     MockMvc mockMvc;
+
     BeerDto beerDto;
 
     @BeforeEach
@@ -48,27 +50,41 @@ class BeerControllerTest {
         beerDto = BeerDto.builder()
             .id(UUID.randomUUID())
             .version(1L)
-            .createdDate(Timestamp.valueOf(LocalDateTime.now()))
-            .lastModifiedDate(Timestamp.valueOf(LocalDateTime.now()))
+            .createdDate(Timestamp.from(Instant.now()))
+            .lastModifiedDate(Timestamp.from(Instant.now()))
             .beerName("Beer1")
             .beerStyle(BeerStyleEnum.PALE_ALE)
             .upc(123456789012L)
             .quantityOnHand(4)
             .price(new BigDecimal("12.99").toString())
             .build();
+    }
 
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    @AfterEach
+    void tearDown() {
+        reset(beerService);
     }
 
     @Test
     void getBeerById() throws Exception {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
         given(beerService.findBeerById(any())).willReturn(beerDto);
 
         mockMvc.perform(get("/api/v1/beer/" + beerDto.getId()))
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$.id", is(beerDto.getId().toString())))
-            .andExpect(jsonPath("$.beerName", is("Beer1")));
+            .andExpect(jsonPath("$.beerName", is("Beer1")))
+            .andExpect(jsonPath("$.createdDate",
+                    is(
+                        formatter.format(
+                                beerDto.getCreatedDate().toInstant().atOffset(ZoneOffset.UTC)
+                            )
+                            .replace("Z", "+00:00")
+                    )
+                )
+            );
         // .andExpect(jsonPath("$.id").value(beerDto.getId().toString()));
     }
 
